521fe1b7c8e2049d083103de9673a667e5e872ef
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-02-08 10:52:54 -0600

    added links to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <h1>Hello from App</h1>
+      <Link to="/profile">Profile</Link>
     </div>
   );
 };
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Profile = () => {
     return (
       <div>
         <h1>Hello from Profile</h1>
+        <Link to="/">Home</Link>
       </div>
     );
   };
